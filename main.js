6549579027c5a91886e8d8ef3a351a829dfb9ccc
Andhana Utama <magicwarms@gmail.com>
2022-07-22 11:17:05 +0700

    feat(blueOcean): blueOcean

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 var SpeSkillTest = /** @class */ (function () {
-    function SpeSkillTest(digits) {
+    function SpeSkillTest(digits, digitsArr) {
         SpeSkillTest.digits = digits;
+        SpeSkillTest.digitsArr = digitsArr;
     }
     SpeSkillTest.prototype.narcissisticNumber = function () {
         var convertToString = SpeSkillTest.digits.toString();
@@ -27,8 +28,26 @@ var SpeSkillTest = /** @class */ (function () {
             return false;
         return arrGenap.length == 1 ? arrGenap[0] : arrGanjil[0];
     };
+    SpeSkillTest.prototype.blueOcean = function () {
+        var numbers1 = SpeSkillTest.digits;
+        var numbers2 = SpeSkillTest.digitsArr;
+        // let result: number[] = [];
+        var removeDuplicate = numbers1.filter(function (item, ind, arr) {
+            return arr.indexOf(item) === arr.lastIndexOf(item);
+        });
+        if (removeDuplicate.length === numbers1.length) {
+            numbers1.shift();
+            return numbers1;
+        }
+        else {
+            return removeDuplicate;
+        }
+    };
     return SpeSkillTest;
 }());
-var skillTest = new SpeSkillTest([160, 3, 1719, 19, 11, 13, -21]);
+// const skillTest = new SpeSkillTest(111);
+// const skillTest = new SpeSkillTest([160, 3, 1719, 19, 11, 13, -21]);
+var skillTest = new SpeSkillTest([1, 2, 3, 4, 6, 10], [5]);
 // console.log({ result: skillTest.narcissisticNumber() });
-console.log({ result: skillTest.parityOutlier() });
+// console.log({ result: skillTest.parityOutlier() });
+console.log({ result: skillTest.blueOcean() });
--- main.ts
+++ main.ts
@@ -1,8 +1,10 @@
 class SpeSkillTest {
   static digits: number | number[];
+  static digitsArr?: number[];
 
-  constructor(digits: number | number[]) {
+  constructor(digits: number | number[], digitsArr?: number[]) {
     SpeSkillTest.digits = digits;
+    SpeSkillTest.digitsArr = digitsArr;
   }
 
   narcissisticNumber(): boolean {
@@ -34,10 +36,26 @@ class SpeSkillTest {
 
     return arrGenap.length == 1 ? arrGenap[0] : arrGanjil[0];
   }
+
+  blueOcean(): number[] {
+    const numbers1 = SpeSkillTest.digits as number[];
+    const removeDuplicate = numbers1.filter((item, ind, arr) => {
+      return arr.indexOf(item) === arr.lastIndexOf(item);
+    });
+
+    if (removeDuplicate.length === numbers1.length) {
+      numbers1.shift();
+      return numbers1;
+    } else {
+      return removeDuplicate;
+    }
+  }
 }
 
 // const skillTest = new SpeSkillTest(111);
-const skillTest = new SpeSkillTest([160, 3, 1719, 19, 11, 13, -21]);
+// const skillTest = new SpeSkillTest([160, 3, 1719, 19, 11, 13, -21]);
+// const skillTest = new SpeSkillTest([1, 2, 3, 4, 6, 10], [5]);
 
 // console.log({ result: skillTest.narcissisticNumber() });
-console.log({ result: skillTest.parityOutlier() });
+// console.log({ result: skillTest.parityOutlier() });
+// console.log({ result: skillTest.blueOcean() });
